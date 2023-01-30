e4c831b95662d127e8a01d27e0992f0ec830d0d0
Hasan6781 <hshaaban597@gmail.com>
2023-01-30 12:31:22 +0200

    first commit

---
+++ Ass2.js
@@ -0,0 +1,11 @@
+function Con() {
+  let a = "Hello ";
+  let b = " World!";
+  return a.concat(" ", b);
+}
+console.log(Con());
+function Replacement() {
+  let c = "Hasan Play football";
+  return c.replaceAll("football", "basketball");
+}
+console.log(Replacement());
